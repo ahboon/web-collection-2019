ac0fe8901421da04102bb6cd17a412d7fcf9c301
CherBoon <simcherboon123@gmail.com>
2019-10-09 08:57:48 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# web-collection-2019
-Web Challenges For 2019
+# Web Challenges
+To provision the web challenges, simply run ``bash web-chall-up.sh``
+
+## Default Ports Exposed:
+- 8181 : BankOfWhitehacks 
+- 8182 : BAsic_SourcErer_85 
+- 8183 : CaptchyCaptcha 
+- 8184 : Commander 
+- 8185 : Commander2 
+- 8186 : Commander3 
+- 8187 : Commander4 
+- 8188 : Commander5 
+- 8189 : GlobalEXTRACTion 
+- 8190 : NiftyHashing 
+- 8191 : Sourcerer 
+- 8192 : truthy 
+
+
---
+++ web-chall-up.sh
@@ -0,0 +1,30 @@
+sudo docker build -t bankofwhitehacks ./BankOfWhitehacks
+sudo docker run -p 8181:80 -t -d bankofwhitehacks
+sudo docker build -t basic_sourcerer_85 ./BAsic_SourcErer_85
+sudo docker run -p 8182:80 -t -d basic_sourcerer_85
+sudo docker build -t captchycaptcha ./CaptchyCaptcha
+sudo docker run -p 8183:80 -t -d captchycaptcha
+sudo docker build -t commander ./Commander
+sudo docker run -p 8184:80 -t -d commander
+sudo docker build -t commander2 ./Commander2
+sudo docker run -p 8185:80 -t -d commander2
+sudo docker build -t commander3 ./Commander3
+sudo docker run -p 8186:80 -t -d commander3
+sudo docker build -t commander4 ./Commander4
+sudo docker run -p 8187:80 -t -d commander4
+sudo docker build -t commander5 ./Commander5
+sudo docker run -p 8188:80 -t -d commander5
+sudo docker build -t globalextraction ./GlobalEXTRACTion
+sudo docker run -p 8189:80 -t -d globalextraction
+sudo docker build -t niftyhashing ./NiftyHashing
+sudo docker run -p 8190:80 -t -d niftyhashing
+sudo docker build -t sourcerer ./Sourcerer
+sudo docker run -p 8191:80 -t -d sourcerer
+sudo docker build -t truthy ./truthy
+sudo docker run -p 8192:80 -t -d truthy
+sudo docker build -t babysql ./baby_sql
+sudo docker run -p 8193:80 -t -d babysql
+sudo docker build -t unionsql ./union_sql
+sudo docker run -p 8194:80 -t -d unionsql
+sudo docker build -t blindsql ./blindsql
+sudo docker run -p 8195:80 -t -d blindsql
